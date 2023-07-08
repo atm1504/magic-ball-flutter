44a17c9cf5cd06ffa67f3faa71bc87b17dd9399c
Amartya Mondal <atm1504.in@gmail.com>
2023-07-08 15:43:13 +0530

    Add widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      const MaterialApp(
-        home: null,
+      MaterialApp(
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade100,
+      appBar: AppBar(
+        title: Text('Ask me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: SafeArea(
+        child: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
+    );
+  }
+}
